c8b8ec7c93eceb3cfec1c8d877cc957c5aff0e4e
Guohao <sccdmaomao@hotmail.com>
2017-12-23 18:52:44 -0500

    add new project

---
+++ DungeonCrawler/DungeonCrawlers.py
@@ -0,0 +1,23 @@
+SWORDS = ["small", "medium", "large"]
+AXE = ["small", "medium", "large"]
+MAGICAL_POTIONS = ["red", "blue", "green"]
+BOWS = ["crossbow", "shortbow", "longbow"]
+LOOT = ["gems", "coins", "artifact"]
+STUFF_THAT_KEEP_YOU_ALIVE = ["food", "healing_potions"]
+MAGICAL_ITEMS = ["wand", "scroll", "ring", "emply"]
+CLASSES = ["fighter", "wizard", "thief", "healer", "ranger"]
+RACES = ["human", "elf", "dwarf", "halfing", "gnome"]
+MISCELLANEOUS_ITEMS = ["torch", "lamp", "key", "map", "empty"]
+
+filename = input("File name to generate the character:(character.txt)")
+
+print(filename)
+
+# Ask for character's lastname + firstname
+# Randomly generate race + class
+# Randomly geenrate stats from 1-20
+# randomly generate weapon in character's back + belt slot
+# randomly generate 1 item from [miscellaneous item, magical item] per hand
+# randomly generate 10 item from [miscellaneous, magical potion, loot, stuff, magical item] for the pack
+
+
